5bdadaddb5dd9667f6a6f96f18aa1526cb57cdc2
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-01-06 20:23:17 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,6 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holiday|
+    season.
     puts "#{season.to_s}:"
     holiday.each do |holiday_name, supplies|
       puts "#{holiday.to_s}: #{supplies}"
